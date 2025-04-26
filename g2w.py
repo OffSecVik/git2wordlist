cdb7dc8e19fdcfaaf706ade2c0b479a0fccfc537
OffSecVik <viktorlinner@gmx.de>
2025-04-26 13:24:09 +0200

    Added file write

--- g2w.py
+++ g2w.py
@@ -3,6 +3,8 @@ from abc import abstractmethod, ABC
 from bs4 import BeautifulSoup
 import requests
 import argparse
+from datetime import datetime
+from urllib.parse import quote
 
 class RepoItem:
     def __init__(self, name, type_, href_, parent):
@@ -21,8 +23,18 @@ class RepoParser(ABC):
         self.auto_encode = auto_encode
 
     @abstractmethod
-    def parseRepo(self):
+    def parseRepo(self, url, parent_directory):
         pass
+
+    def log(self, log_message):
+        if self.verbose:
+            print(log_message)
+
+    def write_result(self, repository_item):
+        if self.auto_encode:
+            repository_item = quote(repository_item)
+        with open(self.outfile, "a") as file:
+            file.write(repository_item + "\n")
 
 class GithubParser(RepoParser):
     def __init__(self, url, outfile, verbose, auto_encode):
@@ -35,11 +47,14 @@ class GithubParser(RepoParser):
         button = soup.find("button", attrs={"id": "branch-picker-repos-header-ref-selector"})
         button_div = button.find("div", attrs={"class": "ref-selector-button-text-container"})
         branch_name = str(button_div.span.text).strip()
+        self.log(f"[+] Found branch: {branch_name}")
         return branch_name
 
     def appendTreeAndBranchName(self, url, branch_name):
         ### MAKE NEW URL WITH COMPLETE BRANCH NAME
-        return url.rstrip("/") + "/tree/" + branch_name
+        new_url = url.rstrip("/") + "/tree/" + branch_name
+        self.log(f"[+] Now crawling: {new_url}")
+        return new_url
 
     def needsBranchName(self, url):
         return not "/tree" in url
@@ -47,11 +62,13 @@ class GithubParser(RepoParser):
     def parseRepo(self, url, parent_directory):
         response = self.session.get(url)
         if response.status_code == 404:
+            self.log(f"[-] Invalid status code for {url}: {response.status_code}")
             return
 
         soup = BeautifulSoup(response.text, 'lxml')
         table = soup.find("table", attrs={"aria-labelledby": "folders-and-files"})
         if not table:
+            self.log(f"[-] Failed to find repository table on url: {url}")
             return
 
         tbody = table.find("tbody")
@@ -64,15 +81,19 @@ class GithubParser(RepoParser):
                 item_info = a.attrs['aria-label'].split(',')
                 if len(item_info) < 2:
                     continue
-                href = a.attrs['href']
 
+                href = a.attrs['href']
                 item_name = item_info[0]
                 item_type = item_info[1].strip()
                 if item_type == "(Directory)":
                     item_name += "/"
                 item = RepoItem(item_name, item_type, href, parent_directory)
-
                 self.repo_items.append(item)
+
+                self.log(f"[+] Found repository item of type {item_type}: {item_name}")
+                self.write_result(item.parent + item.name)
+
+                ### INITIATE RECURSION FOR DIRECTORIES
                 if item.type == "(Directory)":
                     new_url = url.rstrip("/") + "/" + item.name
                     new_parent = parent_directory + item.name
@@ -80,18 +101,17 @@ class GithubParser(RepoParser):
 
     def print_repo_items(self):
         for item in self.repo_items:
-            if item is None:
-                print("NONE ITEM")
-            else:
+            if item is not None:
                 print(item.parent + item.name)
 
     def parse_repo(self):
+        self.log(f"[+] Parsing site: {self.url}")
         url = self.url
         if self.needsBranchName(url):
             branch_name = self.getBranchName(url)
             url = self.appendTreeAndBranchName(url, branch_name)
         self.parseRepo(url, "")
-        self.print_repo_items()
+        # self.print_repo_items()
 
 
 def determineParser(args):
@@ -115,6 +135,11 @@ def main():
 
     args = parser.parse_args()
 
+    if args.outfile is None:
+        now = datetime.now()
+        timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+        args.outfile = f"g2w_{timestamp}.txt"
+
     if parser := determineParser(args):
         parser.parse_repo()
     else:
@@ -123,6 +148,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    ### TODO: Flag: Autourlencode (Should be default behavior)
-    ### TODO: Flag: Verbose (logs finds on the fly, errors too)
 
