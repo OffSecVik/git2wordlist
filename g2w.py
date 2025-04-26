ae322c42da5bcc2e28ca810ab27bce36ec996965
OffSecVik <viktorlinner@gmx.de>
2025-04-26 12:52:47 +0200

    Add classes, abstraction, arguments

--- g2w.py
+++ g2w.py
@@ -1,10 +1,8 @@
+from abc import abstractmethod, ABC
+
 from bs4 import BeautifulSoup
 import requests
 import argparse
-
-repo_items = []
-
-session = requests.session()
 
 class RepoItem:
     def __init__(self, name, type_, href_, parent):
@@ -13,69 +11,115 @@ class RepoItem:
         self.href = href_
         self.parent = parent
 
-def getBranchName(repoUrl):
-    response = session.get(repoUrl)
-    soup = BeautifulSoup(response.text, 'lxml')
-    ### GET BRANCH NAME
-    button = soup.find("button", attrs={"id": "branch-picker-repos-header-ref-selector"})
-    button_div = button.find("div", attrs={"class": "ref-selector-button-text-container"})
-    branch_name = str(button_div.span.text).strip()
-    return branch_name
+class RepoParser(ABC):
+    def __init__(self, url, outfile, verbose, auto_encode):
+        self.repo_items = []
+        self.session = requests.session()
+        self.url = url
+        self.outfile = outfile
+        self.verbose = verbose
+        self.auto_encode = auto_encode
 
-def appendTreeAndBranchName(url, branch_name):
-    ### MAKE NEW URL WITH COMPLETE BRANCH NAME
-    return url + "/tree/" + branch_name
+    @abstractmethod
+    def parseRepo(self):
+        pass
 
-def needsBranchName(url):
-    return not "/tree" in url
+class GithubParser(RepoParser):
+    def __init__(self, url, outfile, verbose, auto_encode):
+        super().__init__(url, outfile, verbose, auto_encode)
 
-def parseRepo(url, parent_directory):
-    response = session.get(url)
-    if response.status_code == 404:
-        return
+    def getBranchName(self, repoUrl):
+        response = self.session.get(repoUrl)
+        soup = BeautifulSoup(response.text, 'lxml')
+        ### GET BRANCH NAME
+        button = soup.find("button", attrs={"id": "branch-picker-repos-header-ref-selector"})
+        button_div = button.find("div", attrs={"class": "ref-selector-button-text-container"})
+        branch_name = str(button_div.span.text).strip()
+        return branch_name
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    table = soup.find("table", attrs={"aria-labelledby": "folders-and-files"})
-    if not table:
-        return
+    def appendTreeAndBranchName(self, url, branch_name):
+        ### MAKE NEW URL WITH COMPLETE BRANCH NAME
+        return url.rstrip("/") + "/tree/" + branch_name
 
-    tbody = table.find("tbody")
-    if not tbody:
-        return
+    def needsBranchName(self, url):
+        return not "/tree" in url
 
-    trows = tbody.find_all("tr")
-    for tr in trows:
-        if a := tr.find("a", attrs={"aria-label": True}):
-            item_info = a.attrs['aria-label'].split(',')
-            if len(item_info) < 2:
-                continue
-            href = a.attrs['href']
+    def parseRepo(self, url, parent_directory):
+        response = self.session.get(url)
+        if response.status_code == 404:
+            return
 
-            item_name = item_info[0]
-            item_type = item_info[1].strip()
-            item = RepoItem(item_name, item_type, href, parent_directory)
+        soup = BeautifulSoup(response.text, 'lxml')
+        table = soup.find("table", attrs={"aria-labelledby": "folders-and-files"})
+        if not table:
+            return
 
-            repo_items.append(item)
-            if item.type == "(Directory)":
-                new_url = url.rstrip("/") + "/" + item.name
-                new_parent = parent_directory + item.name + "/"
-                parseRepo(new_url, new_parent)
+        tbody = table.find("tbody")
+        if not tbody:
+            return
 
-def print_repo_items():
-    for item in repo_items:
-        if item is None:
-            print("NONE ITEM")
-        else:
-            print(item.parent + item.name)
+        trows = tbody.find_all("tr")
+        for tr in trows:
+            if a := tr.find("a", attrs={"aria-label": True}):
+                item_info = a.attrs['aria-label'].split(',')
+                if len(item_info) < 2:
+                    continue
+                href = a.attrs['href']
 
+                item_name = item_info[0]
+                item_type = item_info[1].strip()
+                if item_type == "(Directory)":
+                    item_name += "/"
+                item = RepoItem(item_name, item_type, href, parent_directory)
+
+                self.repo_items.append(item)
+                if item.type == "(Directory)":
+                    new_url = url.rstrip("/") + "/" + item.name
+                    new_parent = parent_directory + item.name
+                    self.parseRepo(new_url, new_parent)
+
+    def print_repo_items(self):
+        for item in self.repo_items:
+            if item is None:
+                print("NONE ITEM")
+            else:
+                print(item.parent + item.name)
+
+    def parse_repo(self):
+        url = self.url
+        if self.needsBranchName(url):
+            branch_name = self.getBranchName(url)
+            url = self.appendTreeAndBranchName(url, branch_name)
+        self.parseRepo(url, "")
+        self.print_repo_items()
+
+
+def determineParser(args):
+    url = args.url
+    verbose = args.verbose
+    outfile = args.outfile
+    auto_encode = args.auto_url_encode
+    if url.startswith("https://github.com"):
+        return GithubParser(url, outfile, verbose, auto_encode)
+    return None
 
 def main():
-    url = "https://github.com/cmsmadesimple/cmsmadesimple"
-    if needsBranchName(url):
-        branch_name = getBranchName(url)
-        url = appendTreeAndBranchName(url, branch_name)
-    parseRepo(url, "")
-    print_repo_items()
+    parser = argparse.ArgumentParser(
+        prog="g2w",
+        description="Parses a git repository and transforms the directory structure into a wordlist.\nCurrently only supports github."
+    )
+    parser.add_argument('url')
+    parser.add_argument('-o', '--outfile')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('-a', '--auto-url-encode', action='store_true')
+
+    args = parser.parse_args()
+
+    if parser := determineParser(args):
+        parser.parse_repo()
+    else:
+        print("Failed to determine version control system")
+
 
 if __name__ == "__main__":
     main()
